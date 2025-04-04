7a408ddd13c30e59813cd783d3631d8505f589bb
Sasha <79007185+Al3ksandr0@users.noreply.github.com>
2025-04-04 15:17:50 +0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import 'antd/dist/reset.css';
-import './styles/style.scss';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-);
